709bacbd45acdc71e502db40aa54dee6ff905a4c
Wang Yingxin <18854803009@163.com>
2020-12-23 14:46:26 +0800

    featureView

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,19 +3,21 @@
     <nav-bar class="home-nav"><template #center>购物街</template></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommand-views :recommends="recommends"></home-recommand-views>
+    <home-feature-view></home-feature-view>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar.vue";
 import homeSwiper from "./childComponents/homeSwiper";
-import homeRecommendViews from "./childComponents/homeRecommendViews"
+import homeRecommendViews from "./childComponents/homeRecommendViews";
+import homeFeatureView from "./childComponents/homeFeatureView";
 
 import { getHomeMultidata } from "@/network/home";
 
 export default {
   name: "Home",
-  components: { NavBar, homeSwiper, homeRecommendViews },
+  components: { NavBar, homeSwiper, homeRecommendViews, homeFeatureView },
   data() {
     return {
       banners: [],
---
+++ src/views/home/childComponents/homeFeatureView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="feature">
+    <a href="">
+      <img src="" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homeFeatureView",
+};
+</script>
+
+<style scoped>
+</style>
